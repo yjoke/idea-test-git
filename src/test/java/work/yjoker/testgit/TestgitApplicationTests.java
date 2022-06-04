1fa199db578f915bf0f81d72eba1af0df618611c
yjoke <sanchuanguli@163.com>
2022-06-04 22:39:04 +0800

    Init

---
+++ src/test/java/work/yjoker/testgit/TestgitApplicationTests.java
@@ -0,0 +1,13 @@
+package work.yjoker.testgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestgitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
